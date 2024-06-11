ee44195cae14af068e8c4f6a472cbd13dcf40cdd
MrJinis <epqlfwls2@gmail.com>
2024-06-11 11:30:12 +0900

    fix : API Call 헤더에 Authorization 추가

--- react/src/pages/admin/AirplaneEdit.js
+++ react/src/pages/admin/AirplaneEdit.js
@@ -12,7 +12,12 @@ function AirplaneEdit() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://${process.env.REACT_APP_RESTAPI_IP}:8080/api/v1/admin/airplane/${id}`)
+    axios.get(`http://${process.env.REACT_APP_RESTAPI_IP}:8080/api/v1/admin/airplane/${id}`, {
+      headers: {
+        'Authorization': "Bearer " + window.localStorage.getItem("accessToken"),
+        'Content-Type': 'application/json'
+      }
+    })
       .then(response => {
         const airplane = response.data.data;
         setAirplaneType(airplane.airplaneType);
